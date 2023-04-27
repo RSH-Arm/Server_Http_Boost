c98213b05432d397ad6a242cab652d4c5c2e3937
_Root <nikolaev-vb@yandex.ru>
2023-04-28 02:39:51 +0300

    Теперь основной обработчик подключений в отдельном потоке. Главный поток программы свободен

--- Test_1/Test_1.cpp
+++ Test_1/Test_1.cpp
@@ -1,7 +1,4 @@
 ﻿
-#define DEBUG false
-
-
 #include <queue>
 #include <iostream>
 #include <fstream> 
@@ -10,9 +7,28 @@
 #include <boost/thread.hpp>
 #include <boost/function.hpp>
 
+#include <boost/lexical_cast.hpp>
+
+
 using namespace std;
 using namespace boost::asio;
 
+class Logger
+{
+	std::ofstream ofs;
+public:
+	Logger() = default;
+
+	Logger(string str_) : ofs(str_)
+	{
+		ofs << "Recording started\n\n";
+	};
+
+	void log(string str, int i)
+	{
+		ofs << "[" << i << "] - " << str << "\n";
+	}
+};
 
 using socket_ptr		= boost::shared_ptr<ip::tcp::socket>;
 using io_service_ptr	= boost::shared_ptr<boost::asio::io_service>;
@@ -21,27 +37,43 @@ using work_ptr			= boost::shared_ptr<boost::asio::io_service::work>;
 
 class Server_Http
 {
-	int										_nThread_Pool;
+	int							_nThread_Pool,
+								_port;
 
-	vector<work_ptr>						_work;
-	vector<boost::thread>					_thread_Pool;
-	vector<io_service_ptr>					_io_service;
+	io_service					_service;
+
+	vector<work_ptr>			_work;
+	vector<boost::thread>		_thread_Pool;
+	vector<io_service_ptr>		_io_service;
+
+	shared_ptr<ip::tcp::acceptor> _acceptor;
+
+	boost::shared_ptr <boost::thread> thread_;
 
 public:
 
-	Server_Http(io_service& service,
-		int					port = 3000,
-		int					tp = boost::thread::hardware_concurrency() - 6)
-	{
-		_nThread_Pool = tp;
-		ip::tcp::acceptor acceptor(service, ip::tcp::endpoint(ip::tcp::v4(), port));
-		listener(service, acceptor);
-	};
+	Server_Http(int	port = 3000, int tp = boost::thread::hardware_concurrency() - 6) :
+		_port(port), _nThread_Pool(tp)
+	{};
 
-private:
-
-	void listener(io_service& service, ip::tcp::acceptor& acceptor)
+	void start()
 	{
+	//	_acceptor = make_shared<ip::tcp::acceptor>(_service, ip::tcp::endpoint(ip::tcp::v4(), _port));
+	//	listener(_service, _acceptor);
+		thread_ = boost::make_shared<boost::thread>(boost::bind(&Server_Http::listener, this));
+	}
+
+	void stop()
+	{
+		_acceptor->close();
+		_service.stop();
+		thread_->join();
+	}
+
+	void listener()
+	{
+		_acceptor = make_shared<ip::tcp::acceptor>(_service, ip::tcp::endpoint(ip::tcp::v4(), _port));
+		
 		_work.reserve(_nThread_Pool);
 		_io_service.reserve(_nThread_Pool);
 		_thread_Pool.reserve(_nThread_Pool);
@@ -56,27 +88,27 @@ private:
 
 		while (true)
 		{
-			socket_ptr sock(new ip::tcp::socket(service));
-			acceptor.accept(*sock);
+			socket_ptr sock(new ip::tcp::socket(_service));
+			_acceptor->accept(*sock);
 
 			balancer(sock);
 		}
 	}
-
+private:
 	void balancer(socket_ptr sock)
 	{
 		static int	nTask = 0,
 			i = 0;
 
-		if (nTask < 100)
+		if (nTask < 2)
 		{
 			_io_service[i]->post(boost::bind(&Server_Http::handler_request, this, sock));
 			nTask++;
 		}
 		else
 		{
-			nTask = 0;
 			i++;
+			nTask = 0;
 			if (i == _nThread_Pool) i = 0;
 		}
 	}
@@ -89,6 +121,21 @@ private:
 		size_t length = sock->read_some(buffer(data), ec); // читаем запрос от клиента
 		if (!ec)
 		{
+			//string s = "TXT_" + boost::lexical_cast<std::string>(boost::this_thread::get_id()) + ".txt";
+			//Logger* lg = new Logger(s);
+			//
+			//string st = "";
+			//st += "id_   - "; 
+			//st += boost::lexical_cast<std::string>(boost::this_thread::get_id());
+			//
+			//lg->log(st, stoi(boost::lexical_cast<std::string>(boost::this_thread::get_id())));
+			//st.clear();
+			// 
+
+			auto idThread = std::stol(to_string(boost::this_thread::get_id()),
+				nullptr,
+				16);
+
 			string message(data, length);
 			string response;
 			if (message.find("GET /home/") != string::npos) {
@@ -105,7 +152,7 @@ private:
 			}
 			else {
 				response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n";
-				response += "<html><body><h1 style=\"text-align:center;\">404 Not Found</h1></body></html>\n";
+				response += "<html><body><h1 style=\"text-align:center;\">404 Not Found - </h1></body></html>\n";
 			}
 
 
@@ -117,8 +164,13 @@ private:
 
 int main()
 {
-	io_service service;
-	Server_Http serv(service, 3000);
+//	io_service service;
+	Server_Http serv(3000);
 
+	serv.start();
+	cout << "1\n";
+	boost::this_thread::sleep(boost::posix_time::seconds(10));
+	cout << "2\n";
+	serv.stop();
 	return 0;
 }
